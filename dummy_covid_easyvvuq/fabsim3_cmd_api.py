c342e91b5d8fc57d454c0f4428fa41c7a7209b82
wedeling <wouteredeling@gmail.com>
2020-05-19 22:17:29 +0200

    restructured directories + added a standard SC campaign

---
+++ dummy_covid_easyvvuq/fabsim3_cmd_api.py
@@ -0,0 +1,50 @@
+# FabSim3 Commands Python API
+#
+# This file maps command-line instructions for FabSim3 to Python functions.
+# NOTE: No effort is made to map output back to FabSim, as this complicates
+# the implementation greatly.
+#
+# This file can be included in any code base. 
+# It has no dependencies, but does require a working FabSim3 installation.
+
+import os
+import sys
+import pytest
+from pprint import pprint
+import subprocess
+import json
+from shutil import copyfile, rmtree
+from sqlalchemy import create_engine
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def fabsim(command, arguments, machine = 'localhost'):
+    """
+    Generic function for running any FabSim3 command.
+    """
+    print('Executing', "fabsim {} {}:{}".format(machine, command, arguments))
+    os.system("fabsim {} {}:{}".format(machine, command, arguments))
+
+def fetch_results(machine='localhost'):
+    fabsim("fetch_results", "", machine)    
+
+def run_uq_ensemble(config, campaign_dir, script, machine='localhost', skip = 0, **args):
+    """
+    Launches a UQ ensemble.
+    """
+    # sim_ID = campaign_dir.split('/')[-1]
+    arguments = "{},campaign_dir={},script={},skip={}".format(config, campaign_dir, script, skip)
+    fabsim("run_adaptive_easyvvuq", arguments, machine=machine)
+    
+def get_uq_samples(config, campaign_dir, number_of_samples, machine = 'localhost'):
+    """
+    Retrieves results from UQ ensemble
+    """
+    # sim_ID = campaign_dir.split('/')[-1]
+    arguments = "{},campaign_dir={},number_of_samples={}".format(config, campaign_dir, number_of_samples)
+    fabsim("get_adaptive_easyvvuq", arguments, machine=machine)
+    
+# def rearrange_files(config):
+#     arguments = "{}".format(config)
+#     fabsim("rearrange_files", arguments, machine="localhost")
