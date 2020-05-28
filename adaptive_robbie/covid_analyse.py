a4bf005fa88240b528625ae8e0d8f1e399f2dfa3
Robert Sinclair <robert.sinclair.15@ucl.ac.uk>
2020-05-28 15:35:07 +0100

    made robbie directory

---
+++ adaptive_robbie/covid_analyse.py
@@ -0,0 +1,42 @@
+"""
+==============================================================================
+ANALYSIS OF THE INITIAL (DIMENSION-ADAPTIVE) STCOHASTIC COLLOCATION CAMPAIGN
+
+Execute once.
+==============================================================================
+"""
+
+import easyvvuq as uq
+import os
+import fabsim3_cmd_api as fab
+
+home = os.path.abspath(os.path.dirname(__file__))
+output_columns = ["cumDeath"]
+work_dir = '/home/wouter/VECMA/Campaigns'
+config = 'UK_easyvvuq_test'
+
+campaign = uq.Campaign(state_file="covid_easyvvuq_state.json", 
+                       work_dir=work_dir)
+print('========================================================')
+print('Reloaded campaign', campaign.campaign_dir.split('/')[-1])
+print('========================================================')
+sampler = campaign.get_active_sampler()
+sampler.load_state("covid_sampler_state.pickle")
+campaign.set_sampler(sampler)
+
+#run the UQ ensemble
+fab.get_uq_samples(config, campaign.campaign_dir, sampler._number_of_samples,
+                   machine='eagle_vecma')
+campaign.collate()
+
+# Post-processing analysis
+analysis = uq.analysis.SCAnalysis(
+    sampler=campaign._active_sampler,
+    qoi_cols=output_columns
+)
+
+campaign.apply_analysis(analysis)
+
+#this is a temporary subroutine which saves the entire state of
+#the analysis in a pickle file. The proper place for this is the database
+analysis.save_state("covid_analysis_state.pickle")
